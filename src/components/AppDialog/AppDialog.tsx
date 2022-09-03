42d3e02a3da7a6391d0e26970f4f07dba2aa7e0b
JP <jpyepezimc@gmail.com>
2022-09-04 01:40:46 +1200

    feat: add dialog

---
+++ src/components/AppDialog/AppDialog.tsx
@@ -0,0 +1,34 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { XMarkIcon } from '@heroicons/react/24/solid'
+import { useState } from 'react'
+
+interface AppDialogProps {
+    trigger: JSX.Element
+}
+
+const AppDialog = ({ trigger }: AppDialogProps) => {
+    const [open, setOpen] = useState(true)
+    return (
+        <Dialog.Root open={open} onOpenChange={setOpen}>
+            {trigger && <Dialog.Trigger>{trigger}</Dialog.Trigger>}
+            <Dialog.Portal>
+                <Dialog.Overlay className="fixed inset-0 bg-neutral-very-dark-black-blue opacity-95" />
+                <Dialog.Close className="w-8 fixed top-8 right-4 pointer-events-none">
+                    <XMarkIcon className="text-white" />
+                </Dialog.Close>
+                <Dialog.Content className="fixed bg-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-8 rounded-lg w-[calc(100%-4rem)]">
+                    <Dialog.Description className="flex flex-col text-primary-very-dark-blue text-center font-semibold gap-4">
+                        <a href="#">Product</a>
+                        <a href="#">Company</a>
+                        <a href="#">About</a>
+                        <a href="#">Team</a>
+                        <a href="#">Gallery</a>
+                        <a href="#">Connect</a>
+                    </Dialog.Description>
+                </Dialog.Content>
+            </Dialog.Portal>
+        </Dialog.Root>
+    )
+}
+
+export default AppDialog
--- src/components/Layout/Appbar.tsx
+++ src/components/Layout/Appbar.tsx
@@ -1,10 +1,11 @@
 import { Bars3Icon } from '@heroicons/react/24/solid'
+import AppDialog from '../AppDialog/AppDialog'
 
 const Appbar = () => {
     return (
         <header className="absolute flex justify-between mt-8 px-4 w-full">
             <h1 className="text-white font-bold text-3xl">Blogr</h1>
-            <Bars3Icon className="w-8 text-white" />
+            <AppDialog trigger={<Bars3Icon className="w-8 text-white" />} />
         </header>
     )
 }
