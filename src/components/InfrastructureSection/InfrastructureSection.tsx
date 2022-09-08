07983a27db2b148b68fe959928a63d8153b3e935
JP Yepez <jp.yepez@roamdigital.com>
2022-09-09 01:04:51 +1200

    feat: add infra button

--- src/components/InfrastructureSection/InfrastructureSection.tsx
+++ src/components/InfrastructureSection/InfrastructureSection.tsx
@@ -1,6 +1,9 @@
 import illustrationPhones from '../../assets/illustration-phones.svg'
+import displayAlert from '../../utils/displayAlert'
 
 const InfrastructureSection = () => {
+    const learnMessage =
+        'You have now learned more... but further down the page.'
     return (
         <div className="flex-col-center bg-gradient-to-b from-body-very-dark-gray-blue to-body-very-dark-desaturated-blue py-24 rounded-tr-3xl rounded-bl-3xl">
             <img
@@ -17,6 +20,13 @@ const InfrastructureSection = () => {
                     ensures your site will load instantly, no matter where your
                     readers are, keeping your site competitive.
                 </p>
+
+                <button
+                    className="rounded-3xl p-4 text-white border border-white font-semibold text-sm leading-[.5] mt-8"
+                    onClick={() => displayAlert(learnMessage)}
+                >
+                    Learn More
+                </button>
             </div>
         </div>
     )
